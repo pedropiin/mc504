6235f265204a9771a59aca3a0bfc87e508530157
pedropiin <pedarosa04@gmail.com>
2024-08-24 21:44:52 -0300

    exp1 finished

--- exp1/simple-shell.c
+++ exp1/simple-shell.c
@@ -14,61 +14,23 @@ Pedro da Rosa Pinheiro - 231081 - Turma A
 /*
 TODO: document function
 */
-int parse_args(char *argv, int size_argv, char **dirs_list) {
-    int last_colon = 0; // position of the last ':' found in the arguments
-    int num_dirs = 0; // number of directories passed as arguments
-    for (int i = 0; i < size_argv; i++) {
-        if (argv[i] == ':') {
-            strncpy(dirs_list[num_dirs], argv + last_colon, (i - last_colon));
-            num_dirs++;
-            last_colon = i + 1;
+int parse_and_crop(char *og_string, int size_og_string, char **arr_strings, char crop_char) {
+    int last_crop_char = 0;
+    int num_elems = 0;
+    for (int i = 0; i < size_og_string; i++) {
+        if (og_string[i] == crop_char) {
+            strncpy(arr_strings[num_elems], og_string + last_crop_char, (i - last_crop_char));
+            num_elems++;
+            last_crop_char = i + 1;
         }
     }
-    //copying last directory
-    strncpy(dirs_list[num_dirs], argv + last_colon, (size_argv - last_colon));
-    num_dirs++;
+    strncpy(arr_strings[num_elems], og_string + last_crop_char, (size_og_string - last_crop_char));
+    num_elems++;
 
-    return num_dirs;
+    return num_elems;
 }
 
-/*
-TODO: document function
-*/
-int parse_input(char *input, int size_input, char **command) {
-    int last_space = 0;
-    int num_arguments = 0;
-    for (int i = 0; i < size_input; i++) {
-        if (input[i] == ' ') {
-            strncpy(command[num_arguments], input + last_space, (i - last_space));
-            num_arguments++;
-            last_space = i + 1;
-        }
-    }
-    strncpy(command[num_arguments], input + last_space, (size_input - last_space));
-    num_arguments++;
-
-    return num_arguments;
-}
-
-int main (int argc, char **argv) {
-    // parse directories as arguments
-        // save position of last ':', initialized with 0
-        // for until end of argv[1]
-        // if ':' is found
-            // new directory is string[old_comma:new:comma]
-    // print simple-shell blablabla
-    // getline or scanf
-    // for through directories 
-        // if directory with file is found (use fopen() without allocation)
-            // save directory
-            // found = 1
-            // break    
-
-    // if found
-        // call execv("prog_name", "arg_list");
-    // else 
-        // print error
-
+void simple_shell(char **argv) {
     // --- Allocating memory for list of directories taken as arguments ---
     int size_dirs = strlen(argv[1]);
     char **dirs_list = (char**)malloc(sizeof(char*) * MAX_STRING_SIZE);
@@ -77,7 +39,7 @@ int main (int argc, char **argv) {
     }
 
     // --- Parsing through arguments and getting directories ---
-    int num_dirs = parse_args(argv[1], size_dirs, dirs_list);
+    int num_dirs = parse_and_crop(argv[1], size_dirs, dirs_list, ':');
 
     // --- Printing shell prompt ---
     printf("simple-shell$: ");
@@ -86,7 +48,7 @@ int main (int argc, char **argv) {
     char *input = (char*)malloc(MAX_COMMAND_SIZE);
     fgets(input, MAX_COMMAND_SIZE, stdin);
 
-    // --- Allocating memory for command and its list of args ---
+    // --- Allocating memory for command and its list of arguments ---
     int size_input = strlen(input);
     char **command = (char**)malloc(sizeof(char*) * MAX_STRING_SIZE);
     for (int i = 0; i < size_input; i++) {
@@ -94,35 +56,41 @@ int main (int argc, char **argv) {
     }
 
     // --- Parsing through input and its arguments ---
-    // here note that the first element/first argument of the command is the command itself
-    int num_args_input =  parse_input(input, size_input, command); 
+    int num_elems_input =  parse_and_crop(input, size_input, command, ' '); 
+    int last_command_size = strlen(command[num_elems_input - 1]);   // Removing '\n' from the end of the last argument
+    command[num_elems_input - 1][last_command_size - 1] = '\0';
 
-    // ---Concatenating the command
-    char first_command [100];
-    strcpy(first_command,"/");
+    // --- Concatenating the command ---
+    char first_command[MAX_STRING_SIZE];
+    strcpy(first_command, "/");
     strcat(first_command, command[0]);
-    char path[100]; 
+
     // --- Checking which of the directories contains the desired command ---
+    char command_path[MAX_STRING_SIZE];
     int found = 0;
     for (int i = 0; i < num_dirs; i++) {
-        strcpy(path, dirs_list[i]);
-        strcat(path,first_command);
-        printf("path: %s", path);
-        if (fopen(path, "r")) {
+        strcpy(command_path, dirs_list[i]);
+        strcat(command_path, first_command);
+        if (fopen(command_path, "r") != NULL) {
             found = 1;
             break;
         } 
     }
-    printf("command[0]: %s\n", command[2]);
+
+    command[num_elems_input] = NULL;    // adding NULL into the end of the list of arguments
+    strcpy(command[0], command_path); // setting the whole path for the command in the list of args used in syscall execv()
     if (found) {
-        printf("exec\n");
-        execv("/usr/bin/ls",command);
+        execv(command_path, command);
     }
     else {
-
+        printf("Error: The input command was not found. Exiting.\n");
     }
+}
 
 
+int main (int argc, char **argv) {
+
+    simple_shell(argv);
 
     return 0;
 }
