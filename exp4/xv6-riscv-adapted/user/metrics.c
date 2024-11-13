914adf9a11748a6364e89e93e556bbe26188a2a8
pedropiin <pedarosa04@gmail.com>
2024-11-13 14:58:14 -0300

    did everything, now its up to dezin

--- exp4/xv6-riscv-adapted/user/metrics.c
+++ exp4/xv6-riscv-adapted/user/metrics.c
@@ -7,16 +7,25 @@
 
 int get_normalized_throughput(int duration, int *throughputs, int round, int *min_throughput, int *max_throughput) {
     // Update min and max throughputs
-    if (throughputs[round] > *max_throughput) *max_throughput = throughputs[round];
-    if (throughputs[round] < *min_throughput) *min_throughput = throughputs[round];
+    printf("entered normalized throughtput with duration = %d, round = %d, min_throughput = %d and max = %d\n", duration, round, *min_throughput, *max_throughput);
+    if (throughputs[round] > *max_throughput) {
+        *max_throughput = throughputs[round];
+    }
+    printf("now max is = %d\n", *max_throughput);
+    if (throughputs[round] < *min_throughput) {
+        *min_throughput = throughputs[round];
+    }
+    printf("now min is = %d\n", *min_throughput);
     // Calculate average throughput
     int sum_throughput = 0;
-    for (int i = 0; i < NUM_ROUNDS; i++) {
+    for (int i = 0; i <= round; i++) {
         sum_throughput += throughputs[i];
     }
-    float average_throughput = (float)sum_throughput / NUM_ROUNDS;
+    printf("sum is = %d\n", sum_throughput);
+    float average_throughput = (float)sum_throughput / (round + 1);
+    printf("avg is = %f\n", average_throughput);
     // Calculate normalized throughput
-    float norm_throughput = 1 - ((average_throughput - *min_throughput) / (float)(*max_throughput - *min_throughput));
-    
+    float norm_throughput = 1 - ((average_throughput - *min_throughput) / (float)(*max_throughput - *min_throughput + 1));
+    printf("calculated\n");
     return norm_throughput;
 }
--- exp4/xv6-riscv-adapted/user/run_experiment.c
+++ exp4/xv6-riscv-adapted/user/run_experiment.c
@@ -6,32 +6,44 @@
 
 #define EXIT_SUCCESS 0
 
-// --- Variables used for throughput calculation ---
-
 
 int main(int argc, char *argv[]) {
     int execs_cpu;
     int execs_io;
 
     // --- Declaring and initializing variables related to the throughput metric ---
-    int * throughputs = malloc(NUM_ROUNDS * sizeof(int));
+    int *throughputs = malloc(NUM_ROUNDS * sizeof(int));
     int max_throughput = 0;
     int min_throughput = __INT32_MAX__;
 
     char file_path[] = "output.txt";
+
+    int sum_throughput = 0;             // Holds the sum of the throughput values through the whole experiment
 
     // --- Executing 30 rounds of the experiment ---
     for (int i = 0; i < NUM_ROUNDS; i++) {
         printf("ROUND: %d\n", i);
         // DO NOT ERASE THE COMENTS BELOW, i commented so i could use smaller numbers and get faster executions
         // also changed num_rounds for 3 instead of 30 
-        execs_cpu = (random() % 9) + 6;
-        execs_io = 20 - execs_cpu;
+
+        // execs_cpu = (random() % 9) + 6;
+        // execs_io = 20 - execs_cpu;
+
+        execs_cpu = random() % 3 + 1;
+        execs_io = 6 - execs_cpu;
         int total_processes = execs_cpu + execs_io;
 
-        int start_time = uptime(); // Record start time of the round
+        int process_count_sec = 0;          // Number of processses completed during one second
+        int num_sec = 0;                    // Number of seconds passed during the experiment
+        int start_time = uptime();          // Record start time of the round
+        int d_time_new;
+        int d_time_old = start_time;        // Number of ticks before one second 
+
+        printf("EXECS CPU: %d / EXECS IO: %d\n", execs_cpu, execs_io);
         while (execs_cpu > 0 || execs_io > 0) {
+
             if (execs_cpu > 0) {
+                // Executing one iteration of CPU-bound
                 int p = fork();
                 int status;
                 if (p == 0) {
@@ -41,9 +53,28 @@ int main(int argc, char *argv[]) {
                 }
                 wait(&status);
                 execs_cpu--;
-                printf("finished for cpu\n");
+                process_count_sec++;
+                printf("finished for cpu now %d\n", execs_cpu);
             }
+
+            // --- Adjusting throughput values ---
+            d_time_new = uptime();
+            if ((d_time_new - d_time_old) > 100) {
+                printf("passed one second with d_time_new = %d and dtime_old = %d\n", d_time_new, d_time_old);
+                // one second passed by
+                d_time_old = d_time_new;
+                num_sec++;
+                if (process_count_sec > max_throughput) {
+                    max_throughput = process_count_sec;
+                }
+                if (process_count_sec < min_throughput) {
+                    min_throughput = process_count_sec;
+                }
+                process_count_sec = 0;
+            }
+
             if (execs_io > 0) {
+                // Executing one iteration of IO-bound
                 int p = fork();
                 int status;
                 if (p == 0) {
@@ -53,18 +84,34 @@ int main(int argc, char *argv[]) {
                 }
                 wait(&status);
                 execs_io--;
-                printf("finished for io\n");
+                process_count_sec++;
+                printf("finished for io now %d\n", execs_io);
+            }
+
+            // --- Adjusting throughput values ---
+            d_time_new = uptime();
+            if ((d_time_new - d_time_old) > 100) {
+                printf("passed one second with d_time_new = %d and dtime_old = %d\n", d_time_new, d_time_old);
+                // one second passed by
+                d_time_old = d_time_new;
+                num_sec++;
+                if (process_count_sec > max_throughput) {
+                    max_throughput = process_count_sec;
+                }
+                if (process_count_sec < min_throughput) {
+                    min_throughput = process_count_sec;
+                }
+                process_count_sec = 0;
             }
         }
-        /// --- START METRICS CALCULATION ---
-        int end_time = uptime(); // Record end time of the round
-        int duration = start_time - end_time;
 
-        /// --- Throughput calculation ---        
-        throughputs[i] = total_processes / duration; // Processes per second
-        float norm_throughput = get_normalized_throughput(duration, throughputs, i, &min_throughput, &max_throughput);
-        printf("VAZAO: %f", norm_throughput);
+        // --- Calculating throughput values of the finished round ---
+        throughputs[i] = total_processes / num_sec;         // Throughput of the round
+        sum_throughput += throughputs[i];                   // Adding the throughput of the round into the sum of throughput values of the experiment
+        int avg_throughput = sum_throughput / (i + 1);      // Adjusting avg_throughput to consider this round
 
+        int norm_throughput = 1 - ((avg_throughput - min_throughput) / (max_throughput - min_throughput));
+        printf("throughput of round %d is = %d\n", i, norm_throughput);
     } 
     free(throughputs);
 
