044f2fa6839affc9dc13e878b018953046aec60a
pedropiin <pedarosa04@gmail.com>
2024-11-16 22:31:20 -0300

    finished

--- exp4/xv6-riscv-adapted/rerun.sh
+++
@@ -1,5 +0,0 @@
-sudo docker rm xv6-risc-container
-sudo docker image rm xv6-risc
-sudo docker build -t xv6-risc .
-sudo docker create -ti --name xv6-risc-container xv6-risc
-sudo docker start -i xv6-risc-container
